c0138943d2d61e1a8cd98e58fa72feb262b82096
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-03-03 02:26:03 +0600

    display details created

--- js/phone-search.js
+++ js/phone-search.js
@@ -12,7 +12,7 @@ const searchPhone = () => {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displaySearchResult(data))
+            .then(data => displaySearchResult(data));
     }
 }
 
@@ -20,17 +20,19 @@ const displayError = () => {
     document.getElementById('error-message').style.display = 'block';
     document.getElementById('error-message').innerText = "No Phone Found !!!";
 }
+
 const displaySearchResult = phones => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     if (phones.length == 0) {
         displayError();
     }
+
     phones.data.forEach(phone => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-        <div onclick="loadPhoneDetail(${phone.slug})" class="card h-100 rounded">
+        <div onclick="loadPhoneDetail('${phone.slug}')" class="card h-100 rounded">
             <img src="${phone.image}" class="card-img-top image-size m-3" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
@@ -41,3 +43,43 @@ const displaySearchResult = phones => {
         searchResult.appendChild(div);
     })
 }
+
+const loadPhoneDetail = slug => {
+    const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayPhoneDetail(data.data));
+    // displayPhoneDetail(data)
+}
+
+const displayPhoneDetail = phone => {
+    console.log(phone)
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card');
+    if (phone.releaseDate = '') {
+        div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top image-size" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">${phone.name}</h3>
+            <h3 class="card-text">${phone.brand}</h3>
+            <h3 class="card-text">"No release date available"</h3> 
+        </div>
+        `;
+        phoneDetails.appendChild(div);
+    }
+    else {
+        div.innerHTML = `
+        <img src="${phone.image}" class="card-img-top image-size" alt="...">
+        <div class="card-body">
+            <h3 class="card-title">${phone.name}</h3>
+            <h3 class="card-text">${phone.brand}</h3>
+            <h3 class="card-text">${phone.releaseDate}</h3> 
+        </div>
+        `;
+        phoneDetails.appendChild(div);
+    }
+}
+
