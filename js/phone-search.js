df16c40912b106e462e2d1902d7b73adea3a598d
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-03-02 21:34:05 +0600

    search bar created

---
+++ js/phone-search.js
@@ -0,0 +1,24 @@
+document.getElementById('error-message').style.display = 'none';
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    // clear data
+    searchField.value = '';
+    // document.getElementById('error-message').style.display = 'none';
+    if (searchText == '') {
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('error-message').innerText = "No Phone Found !!!";
+    }
+    else {
+        // load data
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult())
+            .catch(error => displayError(error));
+    }
+}
+
+const displayError = error => {
+    document.getElementById('error-message').style.display = 'block';
+}
