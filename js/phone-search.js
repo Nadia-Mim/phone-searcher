f2e2a0fe48740d0d9e8dcf5d618d3e88ff889b98
Nadia Tasnim Mim <mimnadiatasnim@gmail.com>
2022-03-07 15:01:08 +0600

    Button updated

--- js/phone-search.js
+++ js/phone-search.js
@@ -1,4 +1,5 @@
 document.getElementById('error-message').style.display = 'none';
+
 const searchPhone = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -33,11 +34,12 @@ const displaySearchResult = phones => {
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
-        <div onclick="loadPhoneDetail('${phone.slug}')" class="card h-100 rounded">
+        <div class="card h-100 rounded">
             <img src="${phone.image}" class="card-img-top image-size m-3" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <h5 class="card-title">${phone.brand}</h5>
+                <p onclick="loadPhoneDetail('${phone.slug}')" class="card-title btn btn-info text-white">Explore</p>
             </div>
         </div>
         `;
@@ -51,11 +53,12 @@ const displaySearchResult = phones => {
                 const div = document.createElement('div');
                 div.classList.add('col');
                 div.innerHTML = `
-                <div onclick="loadPhoneDetail('${phone.slug}')" class="card h-100 rounded">
+                <div class="card h-100 rounded">
                 <img src="${phone.image}" class="card-img-top image-size m-3" alt="...">
-                <div class="card-body">
+                <div class="card-body mt-3">
                     <h5 class="card-title">${phone.phone_name}</h5>
                     <h5 class="card-title">${phone.brand}</h5>
+                    <p onclick="loadPhoneDetail('${phone.slug}')" class="card-title btn btn-info text-white">Explore</p>
                 </div>
                 </div>
                 `;
